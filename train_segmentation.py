60f39d90d12b64b4ed3823aa07a81da3dca6b9de
MYR <loic.themyr@gmail.com>
2020-05-18 11:39:20 +0200

    tmp

--- train_segmentation.py
+++ train_segmentation.py
@@ -13,7 +13,7 @@ from models import get_model
 import torch
 
 import os
-os.environ["CUDA_VISIBLE_DEVICES"] = '1'
+os.environ["CUDA_VISIBLE_DEVICES"] = '0'
 
 def convert_bytes(size):
     for x in ['bytes', 'KB', 'MB', 'GB', 'TB']:
