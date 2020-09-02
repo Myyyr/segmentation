5bdde23ffd9b0b723ad4cba42c96286546c853b3
MYR <loic.themyr@gmail.com>
2020-09-02 14:52:21 +0200

    debug

--- dataio/loader/utils.py
+++ dataio/loader/utils.py
@@ -42,12 +42,12 @@ def write_nifti_img(input_nii_array, meta, savedir):
 
 
 def check_exceptions(image, label=None):
-    if label is not None:
-        if image.shape != label.shape:
-            print('Error: mismatched size, image.shape = {0}, '
-                  'label.shape = {1}'.format(image.shape, label.shape))
-            #print('Skip {0}, {1}'.format(image_name, label_name))
-            raise(Exception('image and label sizes do not match'))
+    # if label is not None:
+    #     if image.shape != label.shape:
+    #         print('Error: mismatched size, image.shape = {0}, '
+    #               'label.shape = {1}'.format(image.shape, label.shape))
+    #         #print('Skip {0}, {1}'.format(image_name, label_name))
+    #         raise(Exception('image and label sizes do not match'))
 
     if image.max() < 1e-6:
         print('Error: blank image, image.max = {0}'.format(image.max()))
