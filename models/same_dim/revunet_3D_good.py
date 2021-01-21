73a05bcd4e5346e871480237f21fb72b78a66045
MYR <loic.themyr@gmail.com>
2021-01-21 10:24:11 +0100

    try no transform - debug

--- models/same_dim/revunet_3D_good.py
+++ models/same_dim/revunet_3D_good.py
@@ -4,7 +4,62 @@ import torch.nn as nn
 import torch.nn.functional as F
 import revtorch.revtorch as rv
 import random
-from models.networks_other import init_weights
+
+id = random.getrandbits(64)
+
+#restore experiment
+#VALIDATE_ALL = False
+#PREDICT = True
+#RESTORE_ID = 7420189804603519207
+#RESTORE_EPOCH = 6
+#LOG_COMETML_EXISTING_EXPERIMENT = ""
+
+#general settings
+SAVE_CHECKPOINTS = False #set to true to create a checkpoint at every epoch
+EXPERIMENT_TAGS = ["bugfreeFinalDrop"]
+EXPERIMENT_NAME = "Reversible NO_NEW60, dropout"
+EPOCHS = 1000
+BATCH_SIZE = 1
+VIRTUAL_BATCHSIZE = 1
+VALIDATE_EVERY_K_EPOCHS = 1
+INPLACE = True
+
+#hyperparameters
+#CHANNELS = [36, 72, 144, 288, 576] #normal doubling strategy
+# CHANNELS = [60, 120, 240, 360, 480]
+CHANNELS = [64, 128, 256, 512, 1024]
+CHANNELS = [int(x/16) for x in CHANNELS]
+INITIAL_LR = 1e-4
+L2_REGULARIZER = 1e-5
+
+#logging settings
+LOG_EVERY_K_ITERATIONS = 5 #0 to disable logging
+LOG_MEMORY_EVERY_K_ITERATIONS = False
+LOG_MEMORY_EVERY_EPOCH = True
+LOG_EPOCH_TIME = True
+LOG_VALIDATION_TIME = True
+LOG_HAUSDORFF_EVERY_K_EPOCHS = 0 #must be a multiple of VALIDATE_EVERY_K_EPOCHS
+LOG_COMETML = False
+LOG_PARAMCOUNT = True
+LOG_LR_EVERY_EPOCH = True
+
+#data and augmentation
+TRAIN_ORIGINAL_CLASSES = False #train on original 5 classes
+DATASET_WORKERS = 1
+SOFT_AUGMENTATION = False #Soft augmetation directly works on the 3 classes. Hard augmentation augments on the 5 orignal labels, then takes the argmax
+NN_AUGMENTATION = True #Has priority over soft/hard augmentation. Uses nearest-neighbor interpolation
+DO_ROTATE = True
+DO_SCALE = True
+DO_FLIP = True
+DO_ELASTIC_AUG = True
+DO_INTENSITY_SHIFT = True
+#RANDOM_CROP = [128, 128, 128]
+
+ROT_DEGREES = 20
+SCALE_FACTOR = 1.1
+SIGMA = 10
+MAX_INTENSITY_SHIFT = 0.1
+
 
 
 class ResidualInner(nn.Module):
@@ -14,22 +69,9 @@ class ResidualInner(nn.Module):
         self.gn = nn.GroupNorm(groups, channels)
         self.conv = nn.Conv3d(channels, channels, 3, padding=1, bias=False)
 
-        for m in self.children():
-            init_weights(m, init_type='kaiming')
-
     def forward(self, x):
-        x = F.leaky_relu(self.gn(self.conv(x)), inplace=True)
+        x = F.leaky_relu(self.gn(self.conv(x)), inplace=INPLACE)
         return x
-
-# class ResidualInner(nn.Module):
-#     def __init__(self, channels, groups):
-#         super(ResidualInner, self).__init__()
-#         # self.gn = nn.BatchNorm3d(channels)
-#         self.conv = nn.Conv3d(channels, channels, 3, padding=1, bias=False)
-
-#     def forward(self, x):
-#         x = F.relu(self.conv(x), inplace=True)
-#         return x
 
 def makeReversibleSequence(channels):
     innerChannels = channels // 2
@@ -45,10 +87,10 @@ def makeReversibleComponent(channels, blockCount):
         modules.append(makeReversibleSequence(channels))
     return rv.ReversibleSequence(nn.ModuleList(modules))
 
-def getChannelsAtIndex(index, channels):
+def getChannelsAtIndex(index):
     if index < 0: index = 0
-    if index >= len(channels): index = len(channels) - 1
-    return channels[index]
+    if index >= len(CHANNELS): index = len(CHANNELS) - 1
+    return CHANNELS[index]
 
 class EncoderModule(nn.Module):
     def __init__(self, inChannels, outChannels, depth, downsample=True):
@@ -57,9 +99,7 @@ class EncoderModule(nn.Module):
         if downsample:
             self.conv = nn.Conv3d(inChannels, outChannels, 1)
         self.reversibleBlocks = makeReversibleComponent(outChannels, depth)
-        for m in self.children():
-            if isinstance(m, nn.Conv3d):
-                init_weights(m, init_type='kaiming')
+
     def forward(self, x):
         if self.downsample:
             x = F.max_pool3d(x, 2)
@@ -74,10 +114,7 @@ class DecoderModule(nn.Module):
         self.upsample = upsample
         if self.upsample:
             self.conv = nn.Conv3d(inChannels, outChannels, 1)
-        for m in self.children():
-            if isinstance(m, nn.Conv3d):
-                init_weights(m, init_type='kaiming')
-                
+
     def forward(self, x):
         x = self.reversibleBlocks(x)
         if self.upsample:
@@ -86,33 +123,29 @@ class DecoderModule(nn.Module):
         return x
 
 class RevUnet3D(nn.Module):
-    def __init__(self, inchannels = 1, channels = [4, 8, 16, 32, 64], out_size = 2, depth = 1, interpolation = None):
+    def __init__(self):
         super(RevUnet3D, self).__init__()
-        self.levels = len(channels)
+        depth = 1
+        self.levels = 5
 
-        self.firstConv = nn.Conv3d(inchannels, channels[0], 3, padding=1, bias=False)
+        self.firstConv = nn.Conv3d(1, CHANNELS[0], 3, padding=1, bias=False)
         #self.dropout = nn.Dropout3d(0.2, True)
-        self.lastConv = nn.Conv3d(channels[0], out_size, 1)#, bias=True)
+        self.lastConv = nn.Conv3d(CHANNELS[0], 2, 1, bias=True)
 
         #create encoder levels
         encoderModules = []
         for i in range(self.levels):
-            encoderModules.append(EncoderModule(getChannelsAtIndex(i - 1, channels), getChannelsAtIndex(i, channels), depth, i != 0))
+            encoderModules.append(EncoderModule(getChannelsAtIndex(i - 1), getChannelsAtIndex(i), depth, i != 0))
         self.encoders = nn.ModuleList(encoderModules)
 
         #create decoder levels
         decoderModules = []
         for i in range(self.levels):
-            decoderModules.append(DecoderModule(getChannelsAtIndex(self.levels - i - 1, channels), getChannelsAtIndex(self.levels - i - 2, channels), depth, i != (self.levels -1)))
+            decoderModules.append(DecoderModule(getChannelsAtIndex(self.levels - i - 1), getChannelsAtIndex(self.levels - i - 2), depth, i != (self.levels -1)))
         self.decoders = nn.ModuleList(decoderModules)
 
 
-        # initialise weights
-        for m in self.modules():
-            if isinstance(m, nn.Conv3d):
-                init_weights(m, init_type='kaiming')
-            
-
+        self.interpolation = nn.Upsample(size = (512,512,256), mode = "trilinear")
 
     def forward(self, x):
         tibo_in_shape = x.shape[-3:]
@@ -132,7 +165,9 @@ class RevUnet3D(nn.Module):
 
         x = self.lastConv(x)
 
-        # x = F.softmax(x, dim=1)
+        # if tibo_in_shape != [512,512,256]:
+        #     x = self.interpolation(x)
+        #x = torch.sigmoid(x)
         return x
 
     @staticmethod
